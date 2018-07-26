2e5869cb58bf2b8c61f4ecb4bad0a9a43e320f1b
Michael Boroski <mjboroski@gmail.com>
2018-07-26 00:50:29 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -21,10 +21,13 @@ Order.defaultProps = {
   cone: true,
   size: 'regular'
 };
- 
+
 Order.propTypes = {
   cone: PropTypes.bool,
   size: PropTypes.string,
   scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
-  orderInfo: PropTypes.object.isRequired
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
 };
